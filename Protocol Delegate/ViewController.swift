2a90a773329cd17cd176e93e085455233e68f25b
Julian Boxnick <j.boxnick@web.de>
2022-09-17 16:12:40 +0200

    Initial Commit

---
+++ Protocol Delegate/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Protocol Delegate
+//
+//  Created by Julian Boxnick on 17.09.22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
